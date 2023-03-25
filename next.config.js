ec2f7525765f59faf1aa4bdd5e0e3cd9eabad1e0
Atta Khan <35913737+Attaullahkhanit@users.noreply.github.com>
2023-03-25 15:01:16 +0500

    share_commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+}
+
+module.exports = nextConfig
+
+
+// module.exports = {
+//   generateBuildId: async () => {
+//     // You can, for example, get the latest git commit hash here
+//     return 'my-build-id'
+//   },
+// }
